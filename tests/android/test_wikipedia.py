946c7f72e6a66425c3507b48ee0eb60da546b806
widowblack11 <oksana.prokopenko16@gmail.com>
2023-03-02 08:40:21 +0300

    fix title of test

--- tests/android/test_wikipedia.py
+++ tests/android/test_wikipedia.py
@@ -20,7 +20,7 @@ def test_search():
         ).should(have.size_greater_than(0))
 
 
-@allure.title("Wikipedia search selene and go to page")
+@allure.title("Wikipedia search python")
 def test_search_selene():
     browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
     browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type("python")
